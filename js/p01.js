9bc7b48646e17716b2e54967a1473dce9e18264e
Kevin Fuentes <alejandroftk2@gmail.com>
2025-01-22 17:11:13 -0700

    cacd

--- js/p01.js
+++ js/p01.js
@@ -14,7 +14,7 @@ const url = "http://jsonplaceholder.typicode.com/albums" ;
 http.open('GET',url,true);
 http.send();
 
-}
+
 http.onreadystatechange = function(){
 // validar la respuesta
 if(this.status ==200 && this.readyState==4){
@@ -48,6 +48,7 @@ if(this.status ==200 && this.readyState==4){
 
 }else mensaje.innerHTML = "surgio un error O Continua"
 
+}
 }
 
 function limpiar(){
